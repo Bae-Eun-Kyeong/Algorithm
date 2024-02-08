c16d9160e0d18713feb87464445e0218f2e52182
배은경 <146847300+Bae-Eun-Kyeong@users.noreply.github.com>
2024-02-08 16:49:45 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,28 @@
+# 15655번 (6)
+
+import sys
+
+n, m = map(int, input().split())
+a = [False]*n
+b = [0]*m
+
+li = list(map(int, input().split()))
+li.sort()
+
+# print(li)
+
+def go(index, n, m, start):
+    if index == m:
+        sys.stdout.write(' '.join(map(str, b)) + '\n')
+        return
+    
+    for i in range(start, n):
+        if a[i] == True:
+            continue
+
+        b[index] = li[i]
+        a[i] = True
+        go(index+1, n, m, i)
+        a[i] = False
+
+go(0, n, m, 0)
